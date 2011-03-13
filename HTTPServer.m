95f7ce9b6b9089cb5ceddc29bc244abe4e09088b
Robbie Hanson <robbiehanson@voalte.com>
2011-03-13 14:08:32 -0400

    A parameter was missing from a log statement, causing a crash.  Thanks to Matt Stevens for the patch.

--- HTTPServer.m
+++ HTTPServer.m
@@ -433,7 +433,7 @@ static const int httpLogLevel = HTTP_LOG_LEVEL_INFO; // | HTTP_LOG_FLAG_TRACE;
 		}
 		else
 		{
-			HTTPLogError(@"%@: Failed to start HTTP Server: %@", err);
+			HTTPLogError(@"%@: Failed to start HTTP Server: %@", THIS_FILE, err);
 			[err retain];
 		}
 		
