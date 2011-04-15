88c887d0fb4d42b88f15af0df9f838f9db6f6efd
Robbie Hanson <robbiehanson@deusty.com>
2011-04-14 23:30:45 -0400

    Performance optimization via issue #46 - "Using this single optimization I got a 26% speed improvement - from 1000req/sec to 3800req/sec."  Special thanks to samuraiblog for doing the benchmarks, tracking down the culprit, and submitting the patch!

--- HTTPConnection.m
+++ HTTPConnection.m
@@ -1834,14 +1834,34 @@ static NSMutableArray *recentNonces;
 **/
 - (NSString *)dateAsString:(NSDate *)date
 {
-	// Example: Sun, 06 Nov 1994 08:49:37 GMT
+	// From Apple's Documentation (Data Formatting Guide -> Date Formatters -> Cache Formatters for Efficiency):
+	// 
+	// "Creating a date formatter is not a cheap operation. If you are likely to use a formatter frequently,
+	// it is typically more efficient to cache a single instance than to create and dispose of multiple instances.
+	// One approach is to use a static variable."
+	// 
+	// This was discovered to be true in massive form via issue #46:
+	// 
+	// "Was doing some performance benchmarking using instruments and httperf. Using this single optimization
+	// I got a 26% speed improvement - from 1000req/sec to 3800req/sec. Not insignificant.
+	// The culprit? Why, NSDateFormatter, of course!"
+	// 
+	// Thus, we are using a static NSDateFormatter here.
 	
-	NSDateFormatter *df = [[[NSDateFormatter alloc] init] autorelease];
-	[df setFormatterBehavior:NSDateFormatterBehavior10_4];
-	[df setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"GMT"]];
-	[df setDateFormat:@"EEE, dd MMM y HH:mm:ss 'GMT'"];
+	static NSDateFormatter *df;
 	
-	// For some reason, using zzz in the format string produces GMT+00:00
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		
+		// Example: Sun, 06 Nov 1994 08:49:37 GMT
+		
+		df = [[NSDateFormatter alloc] init];
+		[df setFormatterBehavior:NSDateFormatterBehavior10_4];
+		[df setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"GMT"]];
+		[df setDateFormat:@"EEE, dd MMM y HH:mm:ss 'GMT'"];
+		
+		// For some reason, using zzz in the format string produces GMT+00:00
+	});
 	
 	return [df stringFromDate:date];
 }
