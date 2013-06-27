12a7bd8b1cfab0bbfb631db20585e546bc19d016
Micha Mazaheri <micha@mazaheri.me>
2013-06-27 22:12:08 +0900

    Initialized variable hasBytesAvailable to fix a warning for OS X target.

--- GCD/GCDAsyncSocket.m
+++ GCD/GCDAsyncSocket.m
@@ -4244,7 +4244,7 @@ enum GCDAsyncSocketConfig
 		return;
 	}
 	
-	BOOL hasBytesAvailable;
+	BOOL hasBytesAvailable = NO;
 	unsigned long estimatedBytesAvailable;
 	
 	if ([self usingCFStreamForTLS])
