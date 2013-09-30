874da491064c8f7d78d8ed8bfe4e34a2f4f64768
Robbie Hanson <robbiehanson@deusty.com>
2013-09-30 11:08:33 -0700

    Bug fix for issue #165 : […] undeclared selector doNothingAtAll:

--- GCD/GCDAsyncSocket.m
+++ GCD/GCDAsyncSocket.m
@@ -6712,6 +6712,9 @@ static OSStatus SSLWriteFunction(SSLConnectionRef connection, const void *data, 
 
 #if TARGET_OS_IPHONE
 
++ (void)ignore:(id)_
+{}
+
 + (void)startCFStreamThreadIfNeeded
 {
 	static dispatch_once_t predicate;
@@ -6734,7 +6737,7 @@ static OSStatus SSLWriteFunction(SSLConnectionRef connection, const void *data, 
 	// So we'll just create a timer that will never fire - unless the server runs for decades.
 	[NSTimer scheduledTimerWithTimeInterval:[[NSDate distantFuture] timeIntervalSinceNow]
 	                                 target:self
-	                               selector:@selector(doNothingAtAll:)
+	                               selector:@selector(ignore:)
 	                               userInfo:nil
 	                                repeats:YES];
 	
