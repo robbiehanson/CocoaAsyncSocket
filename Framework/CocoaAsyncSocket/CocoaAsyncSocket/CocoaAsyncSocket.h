0be3674641339a2fd366e634eca9383145f4283a
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-09-13 01:46:47 -0400

    deleted vendor and added CocoaLumberjack as submodule

---
+++ Framework/CocoaAsyncSocket/CocoaAsyncSocket/CocoaAsyncSocket.h
@@ -0,0 +1,19 @@
+//
+//  CocoaAsyncSocket.h
+//  CocoaAsyncSocket
+//
+//  Created by Alsey Coleman Miller on 9/13/14.
+//  Copyright (c) 2014 Deusty LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaAsyncSocket.
+FOUNDATION_EXPORT double CocoaAsyncSocketVersionNumber;
+
+//! Project version string for CocoaAsyncSocket.
+FOUNDATION_EXPORT const unsigned char CocoaAsyncSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaAsyncSocket/PublicHeader.h>
+
+
---
+++ Framework/CocoaAsyncSocket/CocoaAsyncSocketTests/CocoaAsyncSocketTests.m
@@ -0,0 +1,40 @@
+//
+//  CocoaAsyncSocketTests.m
+//  CocoaAsyncSocketTests
+//
+//  Created by Alsey Coleman Miller on 9/13/14.
+//  Copyright (c) 2014 Deusty LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CocoaAsyncSocketTests : XCTestCase
+
+@end
+
+@implementation CocoaAsyncSocketTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
