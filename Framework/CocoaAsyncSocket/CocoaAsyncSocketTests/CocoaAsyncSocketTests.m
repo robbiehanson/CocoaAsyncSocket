a4ac1bd37eae22b84bbe7de46d6624700b14091a
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-02-05 07:32:24 -0500

    set test build configuration

--- Framework/CocoaAsyncSocket/CocoaAsyncSocketTests/CocoaAsyncSocketTests.m
+++ Framework/CocoaAsyncSocket/CocoaAsyncSocketTests/CocoaAsyncSocketTests.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Deusty LLC. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 #import <XCTest/XCTest.h>
 
 @interface CocoaAsyncSocketTests : XCTestCase
