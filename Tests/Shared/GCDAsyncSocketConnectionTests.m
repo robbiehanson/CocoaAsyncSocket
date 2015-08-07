8b85e7579e87a2b2b9cd79050f5761cf72cc4b5f
Chris Ballinger <chrisballinger@gmail.com>
2015-08-07 11:44:54 -0700

    Move common test to shared directory

--- Tests/Shared/GCDAsyncSocketConnectionTests.m
+++ Tests/Shared/GCDAsyncSocketConnectionTests.m
@@ -1,6 +1,6 @@
 //
-//  CocoaAsyncSocketTestsiOS.m
-//  CocoaAsyncSocketTestsiOS
+//  GCDAsyncSocketConnectionTests.m
+//  GCDAsyncSocketConnectionTests
 //
 //  Created by Christopher Ballinger on 10/31/14.
 //
@@ -8,29 +8,25 @@
 
 #import <Foundation/Foundation.h>
 #import <XCTest/XCTest.h>
-#import "GCDAsyncSocket.h"
-
-#define EXP_SHORTHAND
-#import "Expecta.h"
+@import CocoaAsyncSocket;
 
 static const uint16_t kTestPort = 30301;
-static const NSTimeInterval kAsynchronousTestTimeout = 1.0;
 
-@interface CocoaAsyncSocketTestsiOS : XCTestCase <GCDAsyncSocketDelegate>
+@interface GCDAsyncSocketConnectionTests : XCTestCase <GCDAsyncSocketDelegate>
 @property (nonatomic, strong) GCDAsyncSocket *clientSocket;
 @property (nonatomic, strong) GCDAsyncSocket *serverSocket;
-
 @property (nonatomic, strong) GCDAsyncSocket *acceptedServerSocket;
+
+@property (nonatomic, strong) XCTestExpectation *expectation;
 @end
 
-@implementation CocoaAsyncSocketTestsiOS
+@implementation GCDAsyncSocketConnectionTests
 
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
     self.clientSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
     self.serverSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
-    [Expecta setAsynchronousTestTimeout:kAsynchronousTestTimeout];
 }
 
 - (void)tearDown {
@@ -52,8 +48,12 @@ static const NSTimeInterval kAsynchronousTestTimeout = 1.0;
     success = [self.clientSocket connectToHost:@"127.0.0.1" onPort:kTestPort error:&error];
     XCTAssertTrue(success, @"Client failed connecting to up server socket on port %d %@", kTestPort, error);
     
-    
-    expect(self.acceptedServerSocket).willNot.beNil();
+    self.expectation = [self expectationWithDescription:@"Test Full Connection"];
+    [self waitForExpectationsWithTimeout:30 handler:^(NSError *error) {
+        if (error) {
+            NSLog(@"Error establishing test connection");
+        }
+    }];
 }
 
 #pragma mark GCDAsyncSocketDelegate methods
@@ -69,6 +69,7 @@ static const NSTimeInterval kAsynchronousTestTimeout = 1.0;
  * You may, of course, change this at any time.
  **/
 - (void)socket:(GCDAsyncSocket *)sock didAcceptNewSocket:(GCDAsyncSocket *)newSocket {
+    NSLog(@"didAcceptNewSocket %@ %@", sock, newSocket);
     self.acceptedServerSocket = newSocket;
 }
 
@@ -77,6 +78,8 @@ static const NSTimeInterval kAsynchronousTestTimeout = 1.0;
  * The host parameter will be an IP address, not a DNS name.
  **/
 - (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port {
+    NSLog(@"didConnectToHost %@ %@ %d", sock, host, port);
+    [self.expectation fulfill];
 }
 
 
