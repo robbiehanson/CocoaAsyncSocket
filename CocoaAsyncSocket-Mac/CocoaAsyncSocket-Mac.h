481ed05e5c1f6de0b72d4041c3f258636532f1ec
Jorge Izquierdo <jorge@izqui.me>
2015-09-06 20:06:01 +0200

    Add Mac framework shared schema for building Mac framework from Carthage

---
+++ CocoaAsyncSocket-Mac/CocoaAsyncSocket-Mac.h
@@ -0,0 +1,19 @@
+//
+//  CocoaAsyncSocket-Mac.h
+//  CocoaAsyncSocket-Mac
+//
+//  Created by Jorge Izquierdo on 9/6/15.
+//  Copyright © 2015 Robbie Hanson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CocoaAsyncSocket-Mac.
+FOUNDATION_EXPORT double CocoaAsyncSocket_MacVersionNumber;
+
+//! Project version string for CocoaAsyncSocket-Mac.
+FOUNDATION_EXPORT const unsigned char CocoaAsyncSocket_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaAsyncSocket_Mac/PublicHeader.h>
+
+
