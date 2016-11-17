2d200e5a74c42d92e6c19043e69eadac7d8a6572
Jeff <jeffrey.blayney@gmail.com>
2016-11-16 18:57:10 -0700

    Removed the copyright notice from the CocoaAsyncSocket.h file. The Public Domain License applies to works that "are no longer in copyright term", or "were never protected by copyright law". Removing this copyright effectively allows the library to be fully under the Public Domain License.

--- Source/CocoaAsyncSocket.h
+++ Source/CocoaAsyncSocket.h
@@ -1,9 +1,8 @@
-//
+﻿//
 //  CocoaAsyncSocket.h
 //  CocoaAsyncSocket
 //
 //  Created by Derek Clarkson on 10/08/2015.
-//  Copyright © 2015 Robbie Hanson. All rights reserved.
 //
 
 @import Foundation;
