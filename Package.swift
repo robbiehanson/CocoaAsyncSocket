52327ea50a48e80075ee0b1c5ffd0cfca4a8731f
joelstewart <joel@codeinversion.com>
2019-11-07 12:39:25 -0600

    Adding platforums to package

--- Package.swift
+++ Package.swift
@@ -3,8 +3,15 @@ import PackageDescription
 
 let package = Package(
     name: "CocoaAsyncSocket",
+    platforms: [
+        .macOS(.v10_14),
+        .iOS(.v10),
+        .tvOS(.v10)
+    ],
     products: [
-        .library(name: "CocoaAsyncSocket", targets: ["CocoaAsyncSocket"])
+        .library(
+            name: "CocoaAsyncSocket",
+            targets: ["CocoaAsyncSocket"])
     ],
     targets: [
         .target(
