3d6a2e01e853b515d111d8a9f8f4d794819cc526
Rainer Schlönvoigt <rainer.schlonvoigt@philips.com>
2020-02-12 14:09:29 +0100

    adding swift package manager support

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CocoaAsyncSocket",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "CocoaAsyncSocket",
+            targets: ["CocoaAsyncSocket"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "CocoaAsyncSocket",
+            dependencies: [],
+            path: "Source/GCD")
+    ]
+)
