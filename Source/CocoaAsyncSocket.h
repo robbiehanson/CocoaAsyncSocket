4a7e118332b2d82ac9c30a9695291a57b9afc5ca
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-06-12 22:34:50 +0200

    Silence -Watimport-in-framework-header

--- Source/CocoaAsyncSocket.h
+++ Source/CocoaAsyncSocket.h
@@ -6,7 +6,12 @@
 //  CocoaAsyncSocket project is in the public domain.
 //
 
+#if __has_feature(modules)
+#if __has_warning("-Watimport-in-framework-header")
+#pragma clang diagnostic ignored "-Watimport-in-framework-header"
+#endif
 @import Foundation;
+#endif
 
 //! Project version number for CocoaAsyncSocket.
 FOUNDATION_EXPORT double cocoaAsyncSocketVersionNumber;
