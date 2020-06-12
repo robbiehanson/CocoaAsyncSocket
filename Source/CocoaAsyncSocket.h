dbcb9de0a251be02eeff374c6a5996dc41ed501f
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-06-13 00:21:59 +0200

    Update Source/CocoaAsyncSocket.h
    Co-authored-by: Chris Ballinger <chrisballinger@gmail.com>

--- Source/CocoaAsyncSocket.h
+++ Source/CocoaAsyncSocket.h
@@ -6,12 +6,7 @@
 //  CocoaAsyncSocket project is in the public domain.
 //
 
-#if __has_feature(modules)
-#if __has_warning("-Watimport-in-framework-header")
-#pragma clang diagnostic ignored "-Watimport-in-framework-header"
-#endif
-@import Foundation;
-#endif
+#import <Foundation/Foundation.h>
 
 //! Project version number for CocoaAsyncSocket.
 FOUNDATION_EXPORT double cocoaAsyncSocketVersionNumber;
